60ee130a06376a084a4f6937835b5c00f6a6ae0c
SoaresDevOps <ryan.codes.dev@gmail.com>
2024-11-27 05:27:14 -0300

    Update script.js

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -83,7 +83,7 @@ const handleLogin = (event) => {
     login.style.display = "none"
     chat.style.display = "flex"
 
-    websocket = new WebSocket("ws://localhost:8080")
+    websocket = new WebSocket("wss://https://chat-back-24et.onrender.com")
     websocket.onmessage = processMessage
 }
 
